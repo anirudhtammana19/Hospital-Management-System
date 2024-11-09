b6db6fbfeadb95f34deb0c6ab210d676a3b66e59
'anirudh' <'anirudhtammana434@gmail.com'>
2024-11-09 17:26:33 +0530

    Commit

---
+++ HospitalManagmentSystem/src/main/java/com/HMS/project/HospitalManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.HMS.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagmentSystemApplication.class, args);
+	}
+
+}
