db1a581431172d94d141744092d0b05ef1a442fd
suresh <suresh17prime@gmail.com>
2024-11-16 16:32:57 +0530

    Logging Commit

--- HospitalManagmentSystem/src/main/java/com/hexaware/amazecare/Service/AuthService.java
+++ HospitalManagmentSystem/src/main/java/com/hexaware/amazecare/Service/AuthService.java
@@ -1,5 +1,7 @@
 package com.hexaware.amazecare.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -23,13 +25,18 @@ public class AuthService {
 	@Autowired
 	UserInfoUserDetailsService userService;
 	
+	Logger logger= LoggerFactory.getLogger(AuthService.class);
+	
 	public String authenticateUser(UsersDTO user) {
-		//System.out.println("Provided Details: " + user.getUsername()+"Pass: "+user.getPassword());
+		
+		logger.info("Request initiated to Login by User: "+user.getUsername());
 		Authentication authentication= authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
 				user.getUsername(), user.getPassword()));
 		if(authentication.isAuthenticated()) {
+			logger.info("Login Successfull!");
 			return jwtService.generateToken(userService.loadUserByUsername(user.getUsername()));
 		}else {
+			logger.warn("Login Failed: Invalid Credentials!");
 			throw new UsernameNotFoundException("Invalid Username and Password");
 		}
 	}
