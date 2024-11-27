7f0f53928516d5cf0aee53385089fc409505455d
anirudh <anirudhtammana434@gmail.com>
2024-11-27 12:19:00 +0530

    commiting css and js

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,10 +1,11 @@
+import "./App.css"
+const App = () => {
+  return (
+    <div className="div1">
+      <button className="button">Sign up</button>
+      <button className="button">Log in</button>
+    </div>
+  );
+};
 
-const App=()=>{
-
-    return(
-        <div>
-
-        </div>
-    );
-}
 export default App;
