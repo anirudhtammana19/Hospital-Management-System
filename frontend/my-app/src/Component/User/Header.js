2a9386fffa0e1aeb6cc4e3eb8400e8c00eb2f1cb
suresh <suresh17prime@gmail.com>
2024-11-28 09:11:26 +0530

    Patient

---
+++ frontend/my-app/src/Component/User/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Navbar, Button } from "react-bootstrap";
+
+const Header = ({ userName, userType }) => {
+  return (
+    <Navbar bg="white" className="shadow-sm px-3">
+      <Navbar.Brand className="text-primary fw-bold">Amaze Care</Navbar.Brand>
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text className="me-3">
+          <div>{userName}</div>
+          <small>{userType}</small>
+        </Navbar.Text>
+        <Button variant="outline-danger" size="sm">
+          Logout
+        </Button>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
